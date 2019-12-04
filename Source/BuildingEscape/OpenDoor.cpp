65497faa0029789a4748ff147084f4317f8220b5
Cyle Ven Dawson <dawsonc623@gmail.com>
2019-12-04 17:10:24 -0600

    feat: add as simple room

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,42 @@
+// Copyright Cyle Ven Dawson 2019
+
+
+#include "GameFramework/Actor.h"
+#include "OpenDoor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FRotator Rotation(
+		0.0f,
+		60.0f,
+		0.0f
+	);
+
+	GetOwner()->SetActorRotation(
+		Rotation
+	);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/BuildingEscape/PositionReporter.cpp
@@ -0,0 +1,37 @@
+// Copyright Cyle Ven Dawson 2019
+
+
+#include "GameFramework/Actor.h"
+#include "PositionReporter.h"
+
+// Sets default values for this component's properties
+UPositionReporter::UPositionReporter()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReporter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectLocation = GetOwner()->GetActorLocation().ToString();
+	
+	UE_LOG(LogTemp, Warning, TEXT("%s's position is %s"), *ObjectName, *ObjectLocation);
+}
+
+
+// Called every frame
+void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
